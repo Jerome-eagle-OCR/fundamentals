dae3859431de00be060406e8f06b02f62b534085
Jerome-eagle-OCR <jerome.rigault@expleogroup.com>
2021-03-24 15:46:44 +0100

    Completed class Main - used try and catch inside do while to get integer for sure
    Updated class Addition
    - tranlated params
    - rewrote documentation
    
    Signed-off-by: Jerome-eagle-OCR <jerome.rigault@expleogroup.com>

--- src/jerome/eagle/OCR/Addition.java
+++ src/jerome/eagle/OCR/Addition.java
@@ -2,22 +2,32 @@ package jerome.eagle.OCR;
 
 public class Addition {
 
-    /**
-     * Perform a simple addition
-     * @param entier1 the first number
-     * @param entier2 the second number
+    /*
+      Perform a simple addition between two integers
      */
 
+    /**
+     * Ask for first integer
+     */
     public void displayMessageForEntier1() {
         System.out.println("Entrez le premier entier :");
     }
 
+    /**
+     * Ask for second integer
+     */
     public void displayMessageForEntier2() {
         System.out.println("Entrez le deuxième entier :");
     }
 
-    public int performAddition(int entier1, int entier2) {
-        int addition = entier1 + entier2;
+    /**
+     * Do addition and display result
+     * @param integer1 the first number
+     * @param integer2 the second number
+     */
+
+    public void performAddition(int integer1, int integer2) {
+        int addition = integer1 + integer2;
         System.out.println("Le résultat est " + addition);
     }
 
--- src/jerome/eagle/OCR/Main.java
+++ src/jerome/eagle/OCR/Main.java
@@ -1,8 +1,41 @@
 package jerome.eagle.OCR;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
+       int integer1 = 0;
+       int integer2 = 0;
+       boolean condition;
 
+        Addition addition = new Addition();
+
+        do {
+            try {
+                addition.displayMessageForEntier1();
+                Scanner sc1 = new Scanner(System.in);
+                integer1 = sc1.nextInt();
+                condition = false;
+            } catch (Exception InputMismatchException) {
+                System.out.println("Vous devez entrer un entier");
+                condition = true;
+            }
+        } while (condition);
+
+        do {
+            try {
+                addition.displayMessageForEntier2();
+                Scanner sc2 = new Scanner(System.in);
+                integer2 = sc2.nextInt();
+                condition = false;
+            } catch (Exception InputMismatchException) {
+                System.out.println("Vous devez entrer un entier");
+                condition = true;
+            }
+        } while (condition);
+
+        addition.performAddition(integer1, integer2);
     }
 }
