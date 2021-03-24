35045da9a55e65ed71005f9495209bd943c335f1
Jerome-eagle-OCR <jerome.rigault@expleogroup.com>
2021-03-24 18:46:04 +0100

    Added class Choice
    Updated class Main
    - according to choice instanciate addition or substraction
    - on non available choice ask to launch again application and exit

---
+++ src/jerome/eagle/OCR/Choice.java
@@ -0,0 +1,14 @@
+package jerome.eagle.OCR;
+
+public class Choice {
+    /**
+     * Display all possible operations
+     */
+
+    public void displayPossibleOperations() {
+        System.out.println("Opérations possibles :");
+        System.out.println("1 - addition");
+        System.out.println("2 - soustraction");
+        System.out.println("Quelle opération voulez-vous faire ?");
+    }
+}
--- src/jerome/eagle/OCR/Main.java
+++ src/jerome/eagle/OCR/Main.java
@@ -6,36 +6,73 @@ public class Main {
 
     public static void main(String[] args) {
 
-       int integer1 = 0;
-       int integer2 = 0;
-       boolean condition;
+        int integer1 = 0;
+        int integer2 = 0;
+        boolean condition;
 
-        Addition addition = new Addition();
+        Choice choice = new Choice();
+        choice.displayPossibleOperations();
+        Scanner sc = new Scanner(System.in);
+        int operationChosen = sc.nextInt();
 
-        do {
-            try {
-                addition.displayMessageForEntier1();
-                Scanner sc1 = new Scanner(System.in);
-                integer1 = sc1.nextInt();
-                condition = false;
-            } catch (Exception InputMismatchException) {
-                System.out.println("Vous devez entrer un entier");
-                condition = true;
-            }
-        } while (condition);
+        if (operationChosen == 1) {
+            Addition addition = new Addition();
 
-        do {
-            try {
-                addition.displayMessageForEntier2();
-                Scanner sc2 = new Scanner(System.in);
-                integer2 = sc2.nextInt();
-                condition = false;
-            } catch (Exception InputMismatchException) {
-                System.out.println("Vous devez entrer un entier");
-                condition = true;
-            }
-        } while (condition);
+            do {
+                try {
+                    addition.displayMessageForEntier1();
+                    Scanner sc1 = new Scanner(System.in);
+                    integer1 = sc1.nextInt();
+                    condition = false;
+                } catch (Exception InputMismatchException) {
+                    System.out.println("Vous devez entrer un entier");
+                    condition = true;
+                }
+            } while (condition);
 
-        addition.performAddition(integer1, integer2);
+            do {
+                try {
+                    addition.displayMessageForEntier2();
+                    Scanner sc2 = new Scanner(System.in);
+                    integer2 = sc2.nextInt();
+                    condition = false;
+                } catch (Exception InputMismatchException) {
+                    System.out.println("Vous devez entrer un entier");
+                    condition = true;
+                }
+            } while (condition);
+
+            addition.performAddition(integer1, integer2);
+        }
+        if (operationChosen == 2) {
+            Substraction substraction = new Substraction();
+
+            do {
+                try {
+                    substraction.displayMessageForEntier1();
+                    Scanner sc1 = new Scanner(System.in);
+                    integer1 = sc1.nextInt();
+                    condition = false;
+                } catch (Exception InputMismatchException) {
+                    System.out.println("Vous devez entrer un entier");
+                    condition = true;
+                }
+            } while (condition);
+
+            do {
+                try {
+                    substraction.displayMessageForEntier2();
+                    Scanner sc2 = new Scanner(System.in);
+                    integer2 = sc2.nextInt();
+                    condition = false;
+                } catch (Exception InputMismatchException) {
+                    System.out.println("Vous devez entrer un entier");
+                    condition = true;
+                }
+            } while (condition);
+
+            substraction.performSubstraction(integer1, integer2);
+        } else {
+            System.out.println("Mauvais choix, relancez l'application");}
     }
 }
